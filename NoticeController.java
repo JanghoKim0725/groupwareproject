00d67efa8ec7e7e7ada9e0354035393274cf8c16
JanghoKim0725 <gimj07182@gmail.com>
2025-06-19 21:38:18 +0900

    4days
    4days

--- NoticeController.java
+++ NoticeController.java
@@ -1,7 +1,11 @@
 package com.jpaproject.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import com.jpaproject.entity.NoticeDto;
 import com.jpaproject.service.NoticeService;
 
 @RestController
@@ -13,4 +17,23 @@ public class NoticeController {
 	
 	// 컨트롤러 서비스 실행설정
 	public NoticeController(NoticeService noticeService) {this.noticeService = noticeService;}
+	
+	// 게시판 등록화면 출력
+	@GetMapping("/AdminWrite")
+	public ModelAndView AdminWrite() {
+			
+		ModelAndView model = new ModelAndView();
+		model.setViewName("notice/adminNoticeWrite");
+		return model;
+	}
+		
+	// 게시판 등록,수정,삭제 서비스
+	@PostMapping
+	public String notice(NoticeDto dto1) {
+			
+		String 	  msg = "ok";
+		NoticeDto dto2 = noticeService.notice(dto1);
+		if(dto2 == null) msg = "fail";  
+		return 	  msg;
+	}	
 }
--- NoticeService.java
+++ NoticeService.java
@@ -1,6 +1,11 @@
 package com.jpaproject.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import com.jpaproject.entity.NoticeDto;
 import com.jpaproject.repository.NoticeRepository;
 
 @Service
@@ -11,4 +16,42 @@ public class NoticeService {
 	
 	// 컨트롤러 서비스 실행 설정
 	public NoticeService(NoticeRepository noticeRepository) {this.noticeRepository = noticeRepository;}
+	
+	// 공지사항 목록화면 출력
+	public Page<NoticeDto> List(int page,int size) {
+			
+		// pageable : 페이징 처리를 위한 스프링에서 제공하는 인터페이스 = pageRequest.of(현재페이지번호,출력단위페이지번호,정렬방식)
+		Pageable pageable = PageRequest.of(page,size,Sort.by("ntcno").descending()); 
+		return noticeRepository.findAll(pageable);
+	}
+		
+	// 공지사항 목록화면 총데이터 개수
+	public Long count() {return noticeRepository.count();}
+	
+	// 공지사항 상세보기
+	public NoticeDto detail(int ntcno) {return noticeRepository.findById(ntcno).orElse(null);}
+	
+	// 공지사항 등록,수정,삭제 서비스
+	public NoticeDto notice(NoticeDto dto1) {
+		
+		// 수정하기 상황(바뀐내용 덮어쓰기)
+		if(dto1.getNtcno() > 0 && dto1.getNtctt() != null && !dto1.getNtctt().equals("")) {
+			
+			NoticeDto dto2 = detail(dto1.getNtcno());
+			
+			// 조회수개수 초기화방지 그대로 유지
+			dto1.setNtcht(dto2.getNtcht());
+			
+			// 등록일 초기화방지 그대로 유지
+			dto1.setNtcrd(dto2.getNtcrd());
+		}
+		
+		// 삭제하기 상황
+		if(dto1.getNtcno() > 0 && dto1.getNtctt().equals("")) noticeRepository.deleteById(dto1.getNtcno());
+		
+		// 등록하기 상황
+		else 												  noticeRepository.save(dto1);
+		
+		return dto1;
+	}
 }
